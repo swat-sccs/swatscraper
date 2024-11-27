b317978fd7c54551c91c5390718baf7593b5defd
Damian <37555910+DCRepublic@users.noreply.github.com>
2024-11-27 14:17:34 -0500

    conflict uid

--- main.go
+++ main.go
@@ -170,7 +170,7 @@ func send_to_db(data termData, semester string, year string) {
 															:displayName,
 															:emailAddress,
 															:year) 
-							ON CONFLICT ("bannerId")
+							ON CONFLICT ("uid")
 							DO UPDATE SET 
 								"bannerId" = :bannerId, "displayName"= :displayName, "emailAddress"= :emailAddress , "uid" =:uid RETURNING id;`
 
